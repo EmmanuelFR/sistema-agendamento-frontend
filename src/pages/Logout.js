4e1e08ba53ec1619d504f43e6cc307e275a0f828
EmmanuelFR <emmanuel.unifaa@gmail.com>
2024-10-17 17:03:02 -0300

    :sparkles: Resolvendo o problema de estilo nos ícones do Menu.

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -6,10 +6,6 @@ const Logout = () => {
   return (
     <div className="logout-page-container">
       <header className="logout-header">
-        <div className="menu-icon">
-          <AiOutlineMenu size={28} />
-          <span>Menu</span>
-        </div>
         <img className="logo" src="/images/images-removebg-preview.png" alt="Logo" />
       </header>
 
